1f702585b1535661395499cafa7222d4c1661ea1
mihirs <mihirsonar312@gmail.com>
2026-03-19 15:49:05 +0530

    db fix

--- src/db/connect.js
+++ src/db/connect.js
@@ -1,13 +1,37 @@
 import mongoose from "mongoose";
 
+let cached = global.mongoose;
+
+if (!cached) {
+    cached = global.mongoose = { conn: null, promise: null };
+}
+
 const connectDB = async () => {
+    if (cached.conn) {
+        return cached.conn;
+    }
+
+    if (!cached.promise) {
+        const opts = {
+            bufferCommands: false,
+            maxPoolSize: 10,
+            serverSelectionTimeoutMS: 5000,
+            socketTimeoutMS: 45000,
+        };
+
+        cached.promise = mongoose.connect(process.env.MONGO_URI, opts).then((mongoose) => {
+            console.log("✅MongoDB connected successfully");
+            return mongoose;
+        });
+    }
+
     try {
-        await mongoose.connect(process.env.MONGO_URI)
-        console.log("✅MongoDB connected successfully");
+        cached.conn = await cached.promise;
+        return cached.conn;
     } catch (error) {
         console.error("MongoDB connection failed!!!", error.message);
-        process.exit(1); 
+        throw error;
     }
-}
+};
 
 export default connectDB;
