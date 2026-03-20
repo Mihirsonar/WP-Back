ba5e53e404df7b197f7dff32ceafac96ac93e33c
mihirs <mihirsonar312@gmail.com>
2026-03-20 11:28:55 +0530

    entry fix

--- src/index.js
+++ src/index.js
@@ -1,22 +1,30 @@
-import dotenv from 'dotenv';
-dotenv.config();
+// import dotenv from 'dotenv';
+// dotenv.config();
 import app from './app.js';
 import connectDB from './db/connect.js';
+import serverless from 'serverless-http';
 
-const PORT = process.env.PORT || 3000;
+// const PORT = process.env.PORT || 3000;
 
-const startServer = async () => {
-  try {
-    await connectDB();    
-    app.listen(PORT, () => {
-      console.log(`🚀Server is running on port ${PORT}`);
-    });
-  } catch (error) {
-    console.error('Failed to start server:', error.message);
-    process.exit(1); 
-  } 
+// const startServer = async () => {
+//   try {
+//     await connectDB();    
+//     app.listen(PORT, () => {
+//       console.log(`🚀Server is running on port ${PORT}`);
+//     });
+//   } catch (error) {
+//     console.error('Failed to start server:', error.message);
+//     process.exit(1); 
+//   } 
 
+// };
+
+// startServer();
+
+
+const handler = async(req,res)=>{
+  await connectDB();
+  return app(req,res);
 };
 
-startServer();
-
+export default handler;
