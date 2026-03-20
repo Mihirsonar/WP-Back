dcf74eb393bdbd97aeef56f4265b6c97fd5e37b9
mihirs <mihirsonar312@gmail.com>
2026-03-20 12:31:21 +0530

    asdasd

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ app.use(cookieparser());
 
 app.use(cors({
   origin: 
-    "http://localhost:5173",
+    'http://localhost:5173',
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
   credentials: true
 }));
