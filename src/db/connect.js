250c80d2dc793305adce0899821cac5fde50e06b
mihirs <mihirsonar312@gmail.com>
2026-03-20 13:54:14 +0530

    asda

--- src/db/connect.js
+++ src/db/connect.js
@@ -2,24 +2,18 @@ import mongoose from "mongoose";
 
 const MONGO_URI = process.env.MONGO_URI;
 
-if (!MONGO_URI) {
-  throw new Error("Please define MONGO_URI");
-}
-
 let cached = global.mongoose;
 
 if (!cached) {
   cached = global.mongoose = { conn: null, promise: null };
 }
 
-export default connectDB = async () => {
-  if (cached.conn) {
-    return cached.conn;
-  }
+export const connectDB = async () => {
+  if (cached.conn) return cached.conn;
 
   if (!cached.promise) {
     cached.promise = mongoose.connect(MONGO_URI, {
-      bufferCommands: false, // IMPORTANT
+      bufferCommands: false,
     });
   }
 
