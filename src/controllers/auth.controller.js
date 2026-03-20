35eaf12a26809c5ecdb35a4e183c566e7f9e9251
mihirs <mihirsonar312@gmail.com>
2026-03-20 14:27:00 +0530

    .

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -101,7 +101,7 @@ const login = asyncHandler(async (req, res) => {
   const options = {
     httpOnly: true,
     secure: true,
-    sameSite: "strict",
+    sameSite: "lax",
   };
   return res
     .status(200)
