666e33daefa4f97cd57d63ed3182882ddc5c0249
mihirs <mihirsonar312@gmail.com>
2026-03-20 13:56:21 +0530

    db fixing

--- src/db/connect.js
+++ src/db/connect.js
@@ -1,22 +1,15 @@
 import mongoose from "mongoose";
 
-const MONGO_URI = process.env.MONGO_URI;
 
-let cached = global.mongoose;
 
-if (!cached) {
-  cached = global.mongoose = { conn: null, promise: null };
+const connectDB = async () => {
+    try {
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}`)
+        console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
+    } catch (error) {
+        console.log("MONGODB connection FAILED ", error);
+        process.exit(1)
+    }
 }
 
-export const connectDB = async () => {
-  if (cached.conn) return cached.conn;
-
-  if (!cached.promise) {
-    cached.promise = mongoose.connect(MONGO_URI, {
-      bufferCommands: false,
-    });
-  }
-
-  cached.conn = await cached.promise;
-  return cached.conn;
-};
+export default connectDB
