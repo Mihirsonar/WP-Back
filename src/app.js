472401075e710487282597620af1b86cf0150414
mihirs <mihirsonar312@gmail.com>
2026-03-24 18:24:08 +0530

    adding frontend URL

--- src/app.js
+++ src/app.js
@@ -9,21 +9,42 @@ import connectDB from './db/connect.js';
 
 const app = express();
 
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://work-pilot-front.vercel.app"
+];
+
+let isConnected = false;
+
+const connectOnce = async () => {
+  if (!isConnected) {
+    await connectDB();
+    isConnected = true;
+  }
+};
+
+app.use(async (req, res, next) => {
+  await connectOnce();
+  next();
+});
+
 app.use(express.json());
 app.use(cookieparser());
 
 app.use(cors({
-  origin: 
-    'http://localhost:5173',
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("CORS not allowed"));
+    }
+  },
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
   credentials: true
 }));
 
-// app.options("*", cors());
-app.use(async (req, res, next) => {
-  await connectDB();
-  next();
-});
+app.options("*", cors());
+
 app.use('/api/v1/healthcheck', healthCheckRouter);
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/projects', projectRouter);
@@ -31,9 +52,6 @@ app.use('/api/v1/projects', taskRouter);
 
 app.get('/', (req, res) => {
   res.send('Welcome to the Project Management Application!');
-}); 
+});
 
-// app.get((req,res,error)=>{
-//   res.send()
-// })
 export default app;
