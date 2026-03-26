86285f6726331b0fdf2f5fce4499fdb1c777a97d
mihirs <mihirsonar312@gmail.com>
2026-03-26 12:17:34 +0530

    cors fix

--- src/app.js
+++ src/app.js
@@ -1,18 +1,12 @@
-import express from 'express';
-import cors from 'cors';
-import healthCheckRouter from './routes/healthroute.js';
-import authRouter from './routes/auth.routes.js';
-import projectRouter from './routes/project.routes.js';
-import cookieparser from 'cookie-parser';
-import taskRouter from './routes/task.route.js';
-import connectDB from './db/connect.js';
+import express from "express";
+import healthCheckRouter from "./routes/healthroute.js";
+import authRouter from "./routes/auth.routes.js";
+import projectRouter from "./routes/project.routes.js";
+import cookieparser from "cookie-parser";
+import taskRouter from "./routes/task.route.js";
+import connectDB from "./db/connect.js";
 
 const app = express();
-
-const allowedOrigins = [
-  "http://localhost:5173",
-  "https://work-pilot-front.vercel.app"
-];
 
 let isConnected = false;
 
@@ -31,27 +25,42 @@ app.use(async (req, res, next) => {
 app.use(express.json());
 app.use(cookieparser());
 
-app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("CORS not allowed"));
-    }
-  },
-  methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-  credentials: true
-}));
+app.use((req, res, next) => {
+  const allowedOrigins = [
+    "http://localhost:5173",
+    "https://work-pilot-front.vercel.app"
+  ];
 
-app.options("*", cors());
+  const origin = req.headers.origin;
 
-app.use('/api/v1/healthcheck', healthCheckRouter);
-app.use('/api/v1/auth', authRouter);
-app.use('/api/v1/projects', projectRouter);
-app.use('/api/v1/projects', taskRouter);
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+  }
 
-app.get('/', (req, res) => {
-  res.send('Welcome to the Project Management Application!');
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, PATCH, DELETE, OPTIONS"
+  );
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+
+  next();
+});
+
+app.use("/api/v1/healthcheck", healthCheckRouter);
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/projects", projectRouter);
+app.use("/api/v1/projects", taskRouter);
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the Project Management Application!");
 });
 
 export default app;
