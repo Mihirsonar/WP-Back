ee2771933289043765505950daf09a43459d390d
mihirs <mihirsonar312@gmail.com>
2026-03-31 15:38:35 +0530

    .

--- src/models/task.model.js
+++ src/models/task.model.js
@@ -24,7 +24,7 @@ const taskSchema = new Schema({
     assignedBy:{
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User',
-        required: true
+        // required: true
     },
     status:{
         type: String,
